8856645ca6dba0bc02f545a4f1a65f39e6fa3b8e
Valerie <53797814+valerievazq@users.noreply.github.com>
2021-01-10 15:13:26 -0700

    resource model and router

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,1 +1,16 @@
 // build your `Resource` model here
+const db = require("../data/dbconfig");
+
+function getResources() {
+  return db("resources");
+}
+
+function addResource(resource) {
+  return db("resources").insert(resource);
+}
+
+module.exports = {
+  findById,
+  getResources,
+  addResource,
+};
